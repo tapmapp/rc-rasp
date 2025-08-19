ce1c91fd20f68e91be2e5fd041f85bc8bf2eba63
Francisco Rodriguez-Roca <frrocaguilar@gmail.com>
2025-08-19 22:43:44 +0200

    opt

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import * as dotenv from 'dotenv';
 
 // MODULES
 import { initSocket } from "./Socket";
+import { centerServo } from './servo';
 
 dotenv.config();
 
 
 initSocket();
+
+centerServo();
