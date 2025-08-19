851a2f4f307d30461490fe561c377670e62b23a3
Francisco Rodriguez-Roca <frrocaguilar@gmail.com>
2025-08-19 21:48:04 +0200

    opt

--- src/Socket.ts
+++ src/Socket.ts
@@ -1,13 +1,8 @@
-import * as dotenv from 'dotenv';
-
 import { io, Socket } from "socket.io-client";
 
 export var socket: Socket;
 
 export const initSocket = (): void => {
-
-    console.log(dotenv.config());
-
     console.log('Initialing socket ...', process.env.NEXT_PUBLIC_SOCKET_URL);
 
     socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string, {
@@ -22,12 +17,22 @@ export const initSocket = (): void => {
     const onConnect = () => {
         console.log('Socket connected!');
     };
+
+    const keyUp = () => {
+        console.log('Key UP!');
+    };
+
+    const keyDown = () => {
+        console.log('Key DOWN!');
+    };
     
     const onDisconnect = () => {
         console.log('Socket disconnected!');
     };
 
     socket.on('connect', onConnect);
+    socket.on('keyUp', keyUp);
+    socket.on('keyDown', keyDown);
     socket.on('disconnect', onDisconnect);
 
 };
