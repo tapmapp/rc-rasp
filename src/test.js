58514822108d6acb003b998ace596da15d322814
Francisco Rodriguez-Roca <frrocaguilar@gmail.com>
2025-08-20 03:25:41 +0200

    opt

--- src/test.js
+++ src/test.js
@@ -1,31 +1,19 @@
-const { StreamCamera, Codec, Flip, SensorMode } = require('pi-camera-connect');
+const PiCamera = require('pi-camera');
+const myCamera = new PiCamera({
+  mode: 'video',
+  output: `${ __dirname }/video.h264`,
+  width: 1920,
+  height: 1080,
+  timeout: 5000, // Record for 5 seconds
+  nopreview: true,
+});
 
-async function startStreaming() {
-
-    console.log('running!');
-
-    const streamCamera = new StreamCamera({
-        codec: Codec.MJPEG,
-        flip: Flip.Vertical,
-        sensorMode: SensorMode.Mode6
-    });
-
-    await streamCamera.startCapture();
-
-    cameraStartCapture().then(() => {
-        console.log('Camera is now capturing');
-        streamCamera.on("frame", (framet) => {
-
-            console.log(framet);
-    
-        });
-    });
-
-
-    
-
-}
-
-
-
-startStreaming();
+myCamera.record()
+  .then((result) => {
+    console.log('hi')
+    // Your video was captured
+  })
+  .catch((error) => {
+    console.log(error);
+     // Handle your error
+  });
