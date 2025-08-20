0ead169e99719cbd22c727b0c9e94f5735ae81ca
Francisco Rodriguez-Roca <frrocaguilar@gmail.com>
2025-08-20 03:51:28 +0200

    opt

--- src/test.js
+++ src/test.js
@@ -1,13 +1,28 @@
-import { spawn } from "node:child_process";
+import { StreamCamera, Codec } from 'pi-camera-connect';
+import * as fs from 'fs';
 
-const cam = spawn("rpicam-vid", [
-    "-t", "0",
-    "--width", "1280",
-    "--height", "720",
-    "--framerate", "30",
-    "--profile", "baseline",
-    "--inline",
-    "-o", "-"
-  ]);
-  
-  cam.stderr.on("data", d => console.log(d));
+const runApp = async () => {
+  const streamCamera = new StreamCamera({
+    codec: Codec.H264,
+  });
+
+  const videoStream = streamCamera.createStream();
+
+  const writeStream = fs.createWriteStream('video-stream.h264');
+
+  // Pipe the video stream to our video file
+  videoStream.pipe(writeStream);
+
+  await streamCamera.startCapture();
+
+  // We can also listen to data events as they arrive
+  videoStream.on('data', data => console.log('New data', data));
+  videoStream.on('end', data => console.log('Video stream has ended'));
+
+  // Wait for 5 seconds
+  await new Promise(resolve => setTimeout(() => resolve(), 5000));
+
+  await streamCamera.stopCapture();
+};
+
+runApp();
