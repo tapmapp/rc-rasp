05212db7bcfdf8ea1dc54cdfc2cabbe636ddc459
Francisco Rodriguez-Roca <frrocaguilar@gmail.com>
2025-08-20 04:01:51 +0200

    opt

--- src/stream.js
+++ src/stream.js
@@ -29,56 +29,56 @@ const peerConnection = new RTCPeerConnection(config);
 
 startStreaming(source);
 
-function startStreaming(source) {
+async function startStreaming(source) {
 
   const width = 320;
   const height = 240;
 
-  const camera = new RaspividJpegStream({
+
+  const streamCamera = new StreamCamera({
+    codec: Codec.H264,
     width: width,
-    height: height,
-    timeout: 0,
-    framerate: 24,
-    bitrate: 25000000,
+    height: height
   });
+
+  const videoStream = streamCamera.createStream();
+
+  await videoStream.startCapture();
 
   const canvas = createCanvas(width, height);
   const context = canvas.getContext("2d");
 
   console.log("running!");
 
-  return new Promise((resolve) => {
+  videoStream.on("data", (framet) => {
 
-    camera.on("data", (framet) => {
-
-      console.log(framet);
-      
-      // loadImage(framet).then((image) => {
-
-      //   context.drawImage(image, 0, 0, width, height);
-
-      //   try {
-
-      //     const rgbaFrame = context.getImageData(0, 0, width, height);
-      //     const i420Frame = {
-      //       width,
-      //       height,
-      //       data: new Uint8ClampedArray(1.5 * width * height),
-      //     };
-          
-      //     rgbaToI420(rgbaFrame, i420Frame);
-      //     source.onFrame(i420Frame);
-          
-      //     resolve();
-        
-      //   } catch (error) {
-      //     console.log(error);
-      //     resolve();
-      //   }
-      // });
-    });
+    console.log(framet);
     
-  }).catch((err) => resolve());
+    loadImage(framet).then((image) => {
+
+      context.drawImage(image, 0, 0, width, height);
+
+      try {
+
+        const rgbaFrame = context.getImageData(0, 0, width, height);
+        const i420Frame = {
+          width,
+          height,
+          data: new Uint8ClampedArray(1.5 * width * height),
+        };
+        
+        rgbaToI420(rgbaFrame, i420Frame);
+        source.onFrame(i420Frame);
+        
+        resolve();
+      
+      } catch (error) {
+        console.log(error);
+        resolve();
+      }
+    });
+  });
+
 }
 
 setTimeout(() => {
