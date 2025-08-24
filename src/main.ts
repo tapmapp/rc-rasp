4c1961fbba8936818e43154b92c3be6fa10c3e33
Francisco Rodriguez-Roca <frrocaguilar@gmail.com>
2025-08-25 01:38:23 +0200

    opt

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,24 @@
 import * as dotenv from 'dotenv';
 
 // MODULES
-//import { initSocket } from "./Socket";
+import { initSocket } from "./Socket";
 import { centerServo, setServoAngle } from './servo';
-import * as Stream from './stream.js';
+// import * as Stream from './stream.js';
 
 dotenv.config();
 
 // Stream.startStream();
 
-//const socket = initSocket();
-
-centerServo();
-
-setTimeout(() => {
-    setServoAngle(90)
-}, 3000);
-
-setTimeout(() => {
-    setServoAngle(45)
-}, 6000);
-
-setTimeout(() => {
-    setServoAngle(0)
-}, 9000);
+const socket = initSocket();
 
 const keyUp = (key: string) => {
-    console.log('hi1')
+    centerServo();
 };
 
 const keyDown = (key: string) => {
-    if (key == 'a') console.log('hi2')
-    if (key == 'd') console.log('hi3')
+    if (key == 'a') setServoAngle(0);
+    if (key == 'd') setServoAngle(90);
 };
 
-//socket.on('keyDown', (key: string) => keyDown(key));
-//socket.on('keyUp', (key: string) => keyUp(key));
+socket.on('keyDown', (key: string) => keyDown(key));
+socket.on('keyUp', (key: string) => keyUp(key));
